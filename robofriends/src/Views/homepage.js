1848bdfeba6fb4b949cc3d5cc32c1973049aa68f
Bryan Namoc <bryan.namoc123@gmail.com>
2021-10-26 09:28:41 +0800

    Updating homepage

--- robofriends/src/Views/homepage.js
+++ robofriends/src/Views/homepage.js
@@ -1,26 +1,35 @@
 import logo from '../logo.svg';
-import '../App.css';
+import './homepage.css';
 import React from 'react';
+
+import nana from '../images/avatar1.jpg';
+import guinnevere from '../images/avatar2.jpg';
+import change from '../images/avatar3.jpg';
+import odette from '../images/avatar4.jpg';
 
 class Homepage extends React.Component {
 render (){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and Bryan Namoc.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="Title">
+        <h1 id="pageTitle">Robofriends</h1>
+      </div>
+      <div className="Card">
+        <div className="Cards">
+          <img className="cardImage" src={nana} alt="nana"/>
+        </div>
+        <div className="Cards">
+          <img className="cardImage" src={guinnevere} alt="guinnevere"/>
+        </div>
+        <div className="Cards">
+          <img className="cardImage" src={change} alt="chang-e"/>  
+        </div>
+        <div className="Cards">
+          <img className="cardImage" src={odette} alt="odette"/>  
+        </div>
+      </div>
+    </div> 
+    
   );
 }
 }
